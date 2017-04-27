d9c2720d312d64152a229562e21fdbbe752e532c
Mike Miller <ominestre@gmail.com>
2017-04-27 19:20:14 -0400

    refs #3 by updating css unit tests
    Updates the path because all css test files will exist under a css
    directory.  Also adds a done callback for triggering the end of a test.

--- test/css.js
+++ test/css.js
@@ -5,20 +5,21 @@ const path = require('path');
 describe('CSS task', function(){
     let build = require('../');
 
-    it('Builds a single CSS file as single.min.css', function(){
+    it('Builds a single CSS file as single.min.css', function(done){
         build.css({
-            source: path.resolve(__dirname, './data/single.css'),
+            source: path.resolve(__dirname, './data/css/single.css'),
             name: 'single.css',
             destination: path.resolve(__dirname, './data/results/')
         }).on('end', () => {
             assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/single.min.css')));
+            done();
         });
     });
 
-    it('Builds multiple CSS files as multi.min.css', function(){
+    it('Builds multiple CSS files as multi.min.css', function(done){
         let files = [
-            path.resolve(__dirname, './data/foo.css'),
-            path.resolve(__dirname, './data/bar.css')
+            path.resolve(__dirname, './data/css/foo.css'),
+            path.resolve(__dirname, './data/css/bar.css')
         ];
 
         build.css({
@@ -27,22 +28,24 @@ describe('CSS task', function(){
             destination: path.resolve(__dirname, './data/results')
         }).on('end', () => {
             assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/multi.min.css')));
+            done();
         });
     });
 
-    it('Builds a single CSS file as styles.min.css when no name is provided', function(){
+    it('Builds a single CSS file as styles.min.css when no name is provided', function(done){
         build.css({
-            source: path.resolve(__dirname, './data/single.css'),
+            source: path.resolve(__dirname, './data/css/single.css'),
             destination: path.resolve(__dirname, './data/results/')
         }).on('end', () => {
             assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/styles.min.css')));
+            done();
         });
     });
 
-    it('Builds multiple CSS files as styles.min.css when no name is provided', function(){
+    it('Builds multiple CSS files as styles.min.css when no name is provided', function(done){
         let files = [
-            path.resolve(__dirname, './data/foo.css'),
-            path.resolve(__dirname, './data/bar.css')
+            path.resolve(__dirname, './data/css/foo.css'),
+            path.resolve(__dirname, './data/css/bar.css')
         ];
 
         build.css({
@@ -50,6 +53,7 @@ describe('CSS task', function(){
             destination: path.resolve(__dirname, './data/results')
         }).on('end', () => {
             assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/styles.min.css')));
+            done();
         });
     });
 });
