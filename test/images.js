292115ffd4894893679cbcdadbd8097e7b0f0cdd
Mike Miller <ominestre@gmail.com>
2017-04-27 19:22:17 -0400

    refs #3 by creating test file for image task unit tests

---
+++ test/images.js
@@ -0,0 +1,44 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+
+describe('Image Tasks', function(){
+    let build = require('../');
+
+    it('Moves a single image to the output directory', function(done){
+        build.images({
+            source: path.resolve(__dirname, './data/images/peon.jpg'),
+            destination: path.resolve(__dirname, './data/results/')
+        }).on('end', () => {
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.jpg')));
+            done();
+        });
+    });
+
+    it('Moves multiple images to the output directory using wildcard selectors', function(done){
+        build.images({
+            source: path.resolve(__dirname, './data/images/*.*'),
+            destination: path.resolve(__dirname, './data/results/')
+        }).on('end', () => {
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.jpg')));
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.1.jpg')));
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.2.jpg')));
+            done();
+        });
+    });
+
+    it('wtf does it do for non image files?', function(done){
+        build.images({
+            source: path.resolve(__dirname, './data/images/**/*'),
+            destination: path.resolve(__dirname, './data/results/')
+        }).on('end', () => {
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.jpg')));
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.1.jpg')));
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/peon.2.jpg')));
+            assert.ok(fs.existsSync(path.resolve(__dirname, './data/results/not-image/not-image.js')));
+            done();
+        });
+        assert(false);
+    });
+});
