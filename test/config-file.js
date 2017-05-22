1df817652b1bbc0e9b628faf7b54bb17908718d7
Mike Miller <ominestre@gmail.com>
2017-05-22 17:48:06 -0400

    refs #6 by creating placeholder unit tests

---
+++ test/config-file.js
@@ -0,0 +1,13 @@
+describe('ZugZug initialization and usage tests', () => {
+    xit('Runs tasks normally using the config object if no config file is specified', () => {});
+    xit('Throws an exception when neither a config file or config object is used', () => {});
+    xit(`Throws an exception when an incomplete config file is used and the application can't find a setting for a task`, () => {});
+
+    describe('Using a config file', () => {
+        xit('Constructs and returns an object with the settings of your config file', () => {});
+        xit('Throws an exception when the file does not exist', () => {});
+        xit('Throws an exception when the file cannot be read', () => {});
+        xit('Throws an exception when the file is the wrong filetype', () => {});
+        xit('Overrides the config file when a config object is passed', () => {});
+    });
+});
